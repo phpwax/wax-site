3a5cc94f31fc010db3ec3bcf70f3831ef2a52deb
Sheldon <sheldon.els@gmail.com>
2010-08-10 15:16:44 +0100

    moved to production, don't need redirect on error for dev

--- app/config/production.php
+++ app/config/production.php
@@ -8,3 +8,21 @@ Config::set("log_info", false);
 Config::set("log_warn", false);
 Config::set("log_error", true);
 
+/************ Application Error Handling *******************************************************
+*
+*  When you're running in production mode, you don't want your errors displayed to users.
+*  The following commands can be uncommented to handle errors professionally.
+*  Firstly the routing redirect_on_error gives a location for a 404 error (page not found)
+*  The second redirect_on_error is an application error page.
+*  Both of these can be either actions in your application or static pages.
+*  
+*  Finally email_on_error accepts an email address and email_subject_on_error a text subject.
+*  If these are set a copy of the error trace will be emailed to the address. */
+
+WXRoutingException::$redirect_on_error = "/404.html"; // Page not found error
+
+// Application Error and an email address and subject to send details to.
+WXException::$redirect_on_error = "/error.html";
+//WXException::$email_on_error="";
+//WXException::$email_subject_on_error="";
+/*********************************************************************************************/
