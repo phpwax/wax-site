7604cd2382e0a46fe126c037018e764f039d400b
Mike <michal.noskovic@googlemail.com>
2010-11-11 12:58:11 +0000

    config moved to php file

---
+++ app/config/config.php
@@ -0,0 +1,69 @@
+<?
+//$engine = "BackgroundFile";
+//$life = (60*60*24*365*10); //10 years - will be using expiry queue
+
+Config::$config_array = array(
+  "repo_branch"=>"master",
+  "development"=>array(
+    "db"=>array(
+      "dbtype"=>"mysql",
+      "host"=>"localhost",
+      "database"=>"app_development",
+      "username"=>"root"
+    ),
+  ),
+  "test"=>array(
+    "db"=>array(
+      "dbtype"=>"mysql",
+      "host"=>"localhost",
+      "database"=>"app_test",
+      "username"=>"root"
+    ),
+  ),
+  "production"=>array(
+    "db"=>array(
+      "dbtype"=>"mysql",
+      "host"=>"localhost",
+      "database"=>"app_production",
+      "username"=>"app_production",
+      "password"=>"secret"
+    ),
+  )
+  //,"analytics" => array(
+  //  "email"=>"",
+  //  "password"=> "",
+  //  "id"=> "",
+  //  "tracker_id"=>""
+  //),
+  //"use_expiry_date"=>1,
+  //"twitter"=>array(
+  //  "username"=>""
+  //),
+  //"map_keys"=>array(
+  //  ""=> ""
+  //),
+  //"layout_cache"=>array(
+  //  "lifetime"=>$life,
+  //  "engine"=> $engine,
+  //  "exclude_post"=>"yes",
+  //  "exclusions"=>array(
+  //    "admin"=> "/(admin)/i",
+  //    "cms"=> "#(cms)/#i",      
+  //    "thanks"=> "/.*(thanks).*/i",
+  //    "nocache"=> "/no-wax-cache/i",
+  //    "preview"=> "/preview=/i"
+  //  ),
+  //  "namespace"=>array(
+  //    "#(.*)[/]?#i",
+  //    "#(.*)/(.*)#i"
+  //   ),     
+  //   "disable_sessions" => 1
+  //),
+  //"image_cache"=>array(
+  //  "lifetime"=>"forever",
+  //  "engine"=>"Image",
+  //  "exclude_post"=> "no",
+  //)
+);
+
+?>
