a0ba2b67aa5d74ae0189faf0e71ad61955939d33
mikenosko <michal@oneblackbear.com>
2013-08-20 13:36:42 +0100

    bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+//define("CONTENT_MODEL", "WildfireContent");
+require_once 'vendor/autoload.php';
+AutoLoader::bootstrap();
+require_once 'app/config/environment.php';
+AutoLoader::run_application();
+?>
