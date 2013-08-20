ea2a6ab124df12cfa09276341f078c8c3ed8cbcf
mikenosko <michal@oneblackbear.com>
2013-08-20 13:42:50 +0100

    bootstrap is in root

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,7 @@
 <?php
-//define("CONTENT_MODEL", "WildfireContent");
+#define("CONTENT_MODEL", "WildfireContent");
+#define("URL_MAP_MODEL", "WildfireUrlMap");
+
 require_once 'vendor/autoload.php';
 AutoLoader::bootstrap();
 require_once 'app/config/environment.php';
--- public/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-#define("CONTENT_MODEL", "WildfireContent");
-#define("URL_MAP_MODEL", "WildfireUrlMap");
-
-require_once 'vendor/autoload.php';
-AutoLoader::bootstrap();
-require_once 'app/config/environment.php';
-AutoLoader::run_application();
-?>
